6bc2db6f87ca6fef53df30386da85de761d58562
vishaywesay <evan.matulina@dawsoncollege.qc.ca>
2025-05-05 10:58:08 -0400

    Voltage Read Function Prelim
    Working voltage readout in serial only.

--- voltage_regulator/voltage_regulator.ino
+++ voltage_regulator/voltage_regulator.ino
@@ -1,27 +1,28 @@
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
 #include <OneWire.h>
-#define TempSense 2      // DHT11 data pin connected to Pin 2
-#define Temps // Define sensor type
+#define TempSense 2  // DHT11 data pin connected to Pin 2
+#define Temps        // Define sensor type
 const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);  // Create an LCD object
 int VoltPin = A5;
 
 float temp;
-float VoltValue = analogRead(A5);
-int fan;
-int x;
-int y;
-int z;
-int high;
-int med;
-int low;
+float VoltValue;
+float volt;
+//int fan;
+//int x;
+//int y;
+//int z;
+//int high;
+//int med;
+//int low;
 
 void setup() {
   // put your setup code here, to run once:
-  /*lcd.begin(16, 2);*/           // Initialize LCD type (x columns, y rows)
+  /*lcd.begin(16, 2);*/  // Initialize LCD type (x columns, y rows)
   Serial.begin(9600);
-  pinMode(VoltPin, INPUT); 
+  pinMode(VoltPin, INPUT);
 }
 /*
 void Temper(float temp,float fan){
@@ -39,29 +40,36 @@ void Temper(float temp,float fan){
 }
 */
 float Voltage() {
-  float VoltValue = analogRead(A5);
+  int VoltValue = analogRead(VoltPin);
   //Analog read from the voltage divider using A to D conversion
-  float volt = VoltValue * (50 / 1023.0);
-  //Mapping the voltage 
+  float volt = VoltValue * (5.0 / 1023.0);
+  //Mapping the voltage
+  return volt;
 }
 
-/*void display(float volt, float temp){
+/*
+void display(float volt, float temp) {
   Serial.print("Temperature: ");
   Serial.print(temp);
   Serial.print(" °C");
   lcd.setCursor(0, 8);
-  Serial.print("Voltage: ");
+  Serial.println("Voltage: ");
   Serial.print(volt);
-  Serial.print(" °C");
+  Serial.print("V");
   lcd.setCursor(0, 8);
-  
-
 }
-void loop() {
-  //getVolt(float volt);
-  Temper(temp, fan);
-  lcd.clear();
-  display(volt, temp);
-  
-} */
+*/
 
+void loop() {
+  //float VoltValue = analogRead(VoltPin);
+  //analogRead(VoltPin);
+  VoltValue = Voltage();
+  Serial.print("Voltage:");
+  Serial.print(VoltValue);
+  Serial.println("V");
+  delay(500);
+  //getVolt(float volt);
+  //Temper(temp, fan);
+  lcd.clear();
+ // display(volt);
+}
